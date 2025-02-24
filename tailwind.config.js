3d477fabe5e6571f8ff161083a3a4657f5cb605e
fatemedivan <fatemedivan480@gmail.com>
2025-02-24 19:09:03 +0330

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//tailwind.config.js
+module.export={
+    content:[
+        "./index.html"
+    ],
+    theme:{
+        extend:{
+          fontFamily:{
+            bricolageMed:["bricolageMed" , "sans-serif"]
+          }
+        }
+    }
+}
